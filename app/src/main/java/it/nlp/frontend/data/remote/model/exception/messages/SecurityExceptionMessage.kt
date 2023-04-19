40939b6a8e7cc6b7646b1e2d0aa872d8e6b3ba19
IrinUvis <irinuvis@gmail.com>
2023-04-19 11:06:56 +0200

    Bad credentials message

--- app/src/main/java/it/nlp/frontend/data/remote/model/exception/messages/SecurityExceptionMessage.kt
+++ app/src/main/java/it/nlp/frontend/data/remote/model/exception/messages/SecurityExceptionMessage.kt
@@ -4,7 +4,7 @@ enum class SecurityExceptionMessage(val message: String) {
     InvalidEmailAddress("Email address is invalid"),
     InvalidPassword("Password must be at least 6 characters"),
     EmailAlreadyTaken("There already exists user with entered email: "),
-    BadCredentials("Bad credentials"),
+    BadCredentials("Entered credentials are invalid"),
     NoUserWithPassedId("There is no user with entered ID: "),
     NoUserWithPassedEmail("User with entered email was not found: "),
     TokenDoesNotExist("Entered token does not exist: "),
